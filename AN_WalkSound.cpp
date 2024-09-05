76aa8eb0488ef34f4a72a51640500eae148af01d
HEROSAMA <77721346+HEROSAMA666@users.noreply.github.com>
2024-09-05 21:58:23 +0800

    Add files via upload

---
+++ AN_WalkSound.cpp
@@ -0,0 +1,67 @@
+﻿// Made by Feng Chen aka Peter Feng  :D
+
+
+#include "AN_WalkSound.h"
+#include "Runtime\Engine\Classes\Sound\SoundCue.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "Runtime/Engine/Classes/Kismet/KismetMathlibrary.h"
+
+
+
+void UAN_WalkSound::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+	CollisonQueryParams.AddIgnoredActor(MeshComp->GetOwner());
+	CollisonQueryParams.bReturnPhysicalMaterial = true;
+	CollisonQueryParams.bTraceComplex = true;
+	//👆Ray trace settings
+	if(GEngine)
+	{
+		PlayerLocation = MeshComp->GetOwner()->GetActorLocation();
+		EndLocation = UKismetMathLibrary::MakeVector(PlayerLocation.X, PlayerLocation.Y, PlayerLocation.Z-100.0f);
+		bool bIsHit = GetWorld()->LineTraceSingleByChannel(HitResult,PlayerLocation,EndLocation, ECollisionChannel::ECC_Visibility, CollisonQueryParams);
+		//DrawDebugLine(GetWorld(), PlayerLocation, HitResult.GetActor() ? HitResult.Location : EndLocation, FColor::Red, false, 1.0f);
+		//👆 Draw debug line
+		if(bIsHit)
+		{
+			switch (HitResult.PhysMaterial->SurfaceType)
+			{
+			case 0:
+				// Empty here since default PM type = 0
+				break;
+			case 1:
+				SoundPlay(Sound_1, HitResult.Location);
+				break;
+			case 2:
+				SoundPlay(Sound_2, HitResult.Location);
+				break;
+			case 3:
+				SoundPlay(Sound_3, HitResult.Location);
+				break;
+			case 4:
+				SoundPlay(Sound_4, HitResult.Location);
+				break;
+			default:
+				break;
+			}
+			
+		}
+	}
+}
+//1.Ray trace to get surface type&sound play location
+//2.Play sound at location by using switch phrase
+
+void UAN_WalkSound::SoundPlay(USoundBase* Sound, FVector SoundLoc)
+{
+	if (Sound) 
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, Sound, SoundLoc);
+	}
+}
+//Sound output
+
+void UAN_WalkSound::DebugFunc(int casemnum)
+{
+	GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Blue,FString::Printf(TEXT("Case:%d"),casemnum));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("Surface type:%d"), HitResult.PhysMaterial->SurfaceType));
+}
+//Debug function used to check the surface type during developing
---
+++ AN_WalkSound.h
@@ -0,0 +1,38 @@
+﻿// Made by Feng Chen aka Peter Feng  :D
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_WalkSound.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLOBALGJ_API UAN_WalkSound : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation);//Called when received Anim Notify
+
+	void SoundPlay(USoundBase* Sound,FVector SoundLoc);//Play sound function
+	void DebugFunc(int casenum);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AnimNotify, meta = (DisplayName = "PM_1"))
+		TObjectPtr<USoundBase> Sound_1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AnimNotify, meta = (DisplayName = "PM_2"))
+		TObjectPtr<USoundBase> Sound_2;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AnimNotify, meta = (DisplayName = "PM_3"))
+		TObjectPtr<USoundBase> Sound_3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = AnimNotify, meta = (DisplayName = "PM_4"))
+		TObjectPtr<USoundBase> Sound_4;
+
+	//Add sound effect slots above me👆
+
+	FVector PlayerLocation,EndLocation;
+	FHitResult HitResult;
+	FCollisionQueryParams CollisonQueryParams;
+
+};
